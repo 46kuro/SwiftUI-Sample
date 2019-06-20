4628e7924db85b03dfdcc78e4acfeb81f41bfd21
shinji46kuro <shinji.46.kuro@gmail.com>
2019-06-20 18:40:39 +0900

    HTTPのSampleプロジェクトを作成

---
+++ SwiftUI-HTTPSessionSample/SwiftUI-HTTPSessionSampleTests/SwiftUI_HTTPSessionSampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  SwiftUI_HTTPSessionSampleTests.swift
+//  SwiftUI-HTTPSessionSampleTests
+//
+//  Created by Shinji Kurosawa on 2019/06/20.
+//  Copyright © 2019 Shinji Kurosawa. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftUI_HTTPSessionSample
+
+class SwiftUI_HTTPSessionSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
