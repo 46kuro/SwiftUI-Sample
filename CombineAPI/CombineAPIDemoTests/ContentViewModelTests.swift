807ef24ba84f81187a93bcbf857164712396f714
shinji46kuro <shinji.46.kuro@gmail.com>
2019-10-13 16:57:29 +0900

    ViewModelクラスのテストを実装(テスト結果は出力されないが、実行されていることは確認)

--- CombineAPI/CombineAPIDemoTests/ContentViewModelTests.swift
+++ CombineAPI/CombineAPIDemoTests/ContentViewModelTests.swift
@@ -13,26 +13,41 @@ import XCTest
 
 class ContentViewModelTests: XCTestCase {
 
+    var cancellable: [AnyCancellable] = []
+    
     override func setUp() {
         
     }
 
     override func tearDown() {
-        
+        cancellable.forEach { $0.cancel() }
+        cancellable = []
     }
     
     func testUserPublisher() {
-        MockAPI().publish(request: <#T##Request#>)
+        MockAPI()
+            .publish(request: GithubUserRequest())
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .finished:
+                    XCTAssert(1==1, "completion is called")
+                case .failure:
+                    XCTFail("should success API")
+                }
+            }) { user in
+                XCTAssertEqual(user.totalCount, 5, "user model is received")
+            }
+            .store(in: &cancellable)
     }
     
     class MockAPI: APIProtocol {
         func publish<T>(request: T) -> AnyPublisher<T.Response, Error> where T : Request {
-            return SuccessUserPublisher()
+            return AnyPublisher(SuccessUserPublisher<T>())
         }
     }
     
     struct SuccessUserPublisher<T: Request>: Publisher {
-
+        
         typealias Output = T.Response
         typealias Failure = Error
         
